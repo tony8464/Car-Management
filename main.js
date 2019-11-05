88baff55046f043d0ed82cddb14e4638c9d604a8
tony8464 <46433974+tony8464@users.noreply.github.com>
2019-11-04 23:14:40 -0600

    added disable save button feature & auto calculate feature

--- main.js
+++ main.js
@@ -7,13 +7,38 @@ const estSell = document.getElementById("estSell")
 const estProfit = document.getElementById("estProfit")
 const saveButton = document.getElementById("saveButton")
 const estimatedProfitOutput = document.getElementById("estimatedProfitOutput");
+const form = document.forms["addCarForm"].elements;
 
+function checkForm(){
+let canSubmit = true;
 
-if (make.value == '' & model.value ==''){
-saveButton.disabled = true
+for (let i = 0; i < form.length; i++) {
+    if (form[i].value.length == 0)
+        canSubmit = false
+}
+document.getElementById("saveButton").disabled = !canSubmit;
 }
 function getEstProfit(){    
     const estProf = estSell.value - estParts.value - estLabor.value - buyPrice.value
     estimatedProfitOutput.textContent = "$" + estProf;
 }
-saveButton.addEventListener("click", getEstProfit)
+make.addEventListener("change", checkForm)
+make.addEventListener("keyup", checkForm)
+model.addEventListener("change", checkForm)
+model.addEventListener("keyup", checkForm)
+buyPrice.addEventListener("change", checkForm)
+buyPrice.addEventListener("keyup", checkForm)
+buyPrice.addEventListener("change", getEstProfit)
+buyPrice.addEventListener("keyup", getEstProfit)
+estParts.addEventListener("change", checkForm)
+estParts.addEventListener("keyup", checkForm)
+estParts.addEventListener("change", getEstProfit)
+estParts.addEventListener("keyup", getEstProfit)
+estLabor.addEventListener("change", checkForm)
+estLabor.addEventListener("keyup", checkForm)
+estLabor.addEventListener("change", getEstProfit)
+estLabor.addEventListener("keyup", getEstProfit)
+estSell.addEventListener("change", checkForm)
+estSell.addEventListener("keyup", checkForm)
+estSell.addEventListener("change", getEstProfit)
+estSell.addEventListener("keyup", getEstProfit)
