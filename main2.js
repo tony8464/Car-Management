fb6edc9ad4e6bf16e7e118b150ac4198fce3efe9
tony8464 <46433974+tony8464@users.noreply.github.com>
2019-11-15 19:47:40 -0600

    first commit

---
+++ main2.js
@@ -0,0 +1,6 @@
+getCarDataJSON();
+async function getCarDataJSON() {
+    const response = await fetch('cars.json')
+    const carData = await response.json()
+    document.getElementById('data-dump').innerHTML = carData
+}
