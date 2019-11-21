50badf8101321984955a4e2471baed544ea51026
tony8464 <46433974+tony8464@users.noreply.github.com>
2019-11-20 22:04:10 -0600

    started for loop to show/hide pages

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ const saveButton = document.getElementById("saveButton")
 const estimatedProfitOutput = document.getElementById("estimatedProfitOutput");
 const form = document.forms["addCarForm"].elements;
 
-getCarDataJSON();
+/*getCarDataJSON();
 async function getCarDataJSON() {
-const response = await fetch('cars.json')
-const carData = await response.carData
-document.getElementById('estimatedProfitOutput') = carData
+    const response = await fetch('cars.json')
+    const carData = await response.carData
+    document.getElementById('estimatedProfitOutput') = carData;
 
-}
+}*/
 function checkForm() {
     let canSubmit = true;
 
@@ -46,19 +46,33 @@ allInputs.forEach(function (input) {
     input.addEventListener("keyup", getEstProfit)
 
 })
-
+/*
 // this is how the Array.forEach method is implemented
 function forEach(array, callbackFunction) {
     for (let i = 0; i < array.length; i++) {
         callbackFunction(array[i])
     }
+}*/
+const pages = ["0123", "234"]
+//document.getElementById('add-car-page').style.display = 'none';
+function showHidePage() {
+    const pages = [document.getElementById('add-car-page'), document.getElementById('overview-page')];
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+} \
+
+
+showHidePage();
+/*
+function showHide(d) {
+    var onediv = document.getElementById(d);
+    var divs = ['content1', 'content2', 'content3', 'content4'];
+    for (var i = 0; i < divs.length; i++) {
+        if (onediv != document.getElementById(divs[i])) {
+            document.getElementById(divs[i]).style.display = 'none';
+        }
+    }
+    onediv.style.display = 'block';
 }
-/*buyPrice.addEventListener("change", getEstProfit)
-buyPrice.addEventListener("keyup", getEstProfit)
-estParts.addEventListener("change", getEstProfit)
-estParts.addEventListener("keyup", getEstProfit)
-estLabor.addEventListener("change", getEstProfit)
-estLabor.addEventListener("keyup", getEstProfit)
-estSell.addEventListener("change", getEstProfit)
-estSell.addEventListener("keyup", getEstProfit)
 */
