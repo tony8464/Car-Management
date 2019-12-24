c90b394fca2db3aaa5be5bfc63dff5c09966d575
tony8464 <46433974+tony8464@users.noreply.github.com>
2019-12-23 22:59:27 -0600

    rebuilt filters

--- overview.js
+++ overview.js
@@ -2,27 +2,36 @@ const carTable = document.querySelector('#car-table')
 const hideSoldCarsCheckbox = document.getElementById('hide-sold-cars')
 const makeSelect = document.getElementById('make-select')
 
-hideSoldCarsCheckbox.addEventListener('change', function () {
-    const soldCars = getSoldCars()
-    if (hideSoldCarsCheckbox.checked) {
-        soldCars.forEach(hideElement)
-    }
-    else {
-        soldCars.forEach(showElement)
-    }
-})
+let carData = []
+
 function showElement(element) {
     element.classList.remove('u-hidden')
 }
 function hideElement(element) {
     element.classList.add('u-hidden')
 }
-function getSoldCars() {
-    return document.querySelectorAll('.car-sold')
+function removeAllRows() {
+    const allRows = carTable.querySelectorAll('tr')
+    allRows.forEach(function (trElement) {
+        trElement.remove()
+    })
 }
 
+function isCarFilteredOut(car) {
+    if (car.status === 'sold' && hideSoldCarsCheckbox.checked) {
+        return true
+    } else if (car.make !== makeSelect.value && makeSelect.value !== 'all') {
+        return true
+    } else {
+        return false
+    }
+}
 
 function renderCarRow(car) {
+    const shouldSkipCar = isCarFilteredOut(car)
+    if (shouldSkipCar === true) {
+        return
+    }
     const row = document.createElement('tr')
 
     // make column
@@ -44,10 +53,6 @@ function renderCarRow(car) {
     statusColumn.textContent = car.status
     row.appendChild(statusColumn)
 
-    if (car.status === 'sold') {
-        row.classList.add('car-sold')
-    }
-    row.classList.add('car-make-' + car.make)
 
     carTable.appendChild(row)
 }
@@ -56,8 +61,8 @@ function renderCarRow(car) {
 getCarDataJSON();
 async function getCarDataJSON() {
     const response = await fetch('cars.json')
-    const carData = await response.json()
-    carData.forEach(renderCarRow)
+    carData = await response.json()
+    renderRows()
 
     const makeOptions = []
     carData.forEach(function (car) {
@@ -78,18 +83,14 @@ function addMakeOption(make) {
     makeSelect.appendChild(option)
 }
 
-makeSelect.addEventListener('change', function () {
-    const allRows = carTable.querySelectorAll('tr')
-    if (makeSelect.value === 'all') {
-        allRows.forEach(showElement)
-    } else {
-        const expectedClass = 'car-make-' + makeSelect.value
-        allRows.forEach(function (row) {
-            if (row.classList.contains(expectedClass)) {
-                showElement(row)
-            } else {
-                hideElement(row)
-            }
-        })
-    }
-})
+function renderRows() {
+    carData.forEach(renderCarRow)
+}
+
+function reRenderRows() {
+    removeAllRows()
+    renderRows()
+}
+
+makeSelect.addEventListener('change', reRenderRows)
+hideSoldCarsCheckbox.addEventListener('change', reRenderRows)
