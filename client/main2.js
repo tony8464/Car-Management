bae34bdd9b6c2f9fd7287828ef71a4b5d83709ea
tony8464 <46433974+tony8464@users.noreply.github.com>
2020-03-14 14:20:27 -0500

    delete main2.html

--- client/main2.js
+++
@@ -1,6 +0,0 @@
-/*getCarDataJSON();
-async function getCarDataJSON() {
-    const response = await fetch('cars.json')
-    const carData = await response.json()
-    document.getElementById('data-dump').innerHTML = carData
-}*/
